f6385da0eb226e3ee30a5939deb97f8e0c140060
John Saylor <johnmsaylor@gmail.com>
2022-01-25 22:18:25 -0500

    sum odds

--- src/main/scala/functional/easy/SumOdds.scala
+++ src/main/scala/functional/easy/SumOdds.scala
@@ -2,13 +2,21 @@ package org.saylor.hackerrank
 package functional.easy
 
 object SumOdds extends App{
-  val arr: List[Int] = List(3,2,2,3)
   val so: SumOdds = new SumOdds
-  println(so.f2(arr))
+  val arr1: List[Int] = List(3,2,2,3)
+  val arr2: List[Int] = List(11,25,18,-1,26,-20,-19,23,-24,-8)
+
+  val arr = arr2
+  println(so.util(arr))
+  println(so.f(arr))
 }
 
 class SumOdds {
-  def f(arr:List[Int]):Int = arr.filter(_ % 2 == 1).sum
 
-  def f2(arr:List[Int]):Int = arr.filter(a => a % 2 == 1).foldLeft(0)((z,a) => z + a)
+  def util(arr: List[Int]): List[Int] = arr.filter(a => (a ^ 1) != a + 1)
+
+  def f(arr:List[Int]):Int = arr.filter(a => (a ^ 1) != a + 1).sum
+
+  def f2(arr:List[Int]):Int = arr.filter(a => (a ^ 1) != a + 1).foldLeft(0)((z,a) => z + a)
+
 }
